847fad973d00f7dc33e181fc08b12c095add14ef
Rhys Smith <rhys.smith@audacia.co.uk>
2022-08-15 16:21:27 +0100

    feat: commit for switching repos

---
+++ APITestingTemplate/APITestingTemplate/Helpers/Resources.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APITestingTemplate.Helpers
+{
+    public static class Resources
+    {
+        // Book
+        public static string GetBookById = "Book";
+    }
+}
---
+++ APITestingTemplate/APITestingTemplate/Tests/ExampleTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using APITestingTemplate.Helpers;
+using APITestingTemplate.Models.Dtos;
+using Audacia.Testing.Api;
+using FluentAssertions;
+using Xunit;
+
+
+namespace APITestingTemplate.Tests
+{
+    public class 
+        
+        ExampleTests : ApiTestsBase
+    {
+
+        [Fact]
+        public void Scenario_1_As_a_user_I_can_get_a_book_by_its_Id()
+        {
+            // Set the bookId you wish to get
+            var bookId = Constants.TheManWhoDiedTwiceBookID;
+
+            // Call the get API to get the book by its ID
+            var getBookResponse = Get<BookCommandResult>(bookId, Resources.GetBookById);
+
+            // Check the status code is ok
+            getBookResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            // Check that the correct book is returned
+            getBookResponse.Data.Output.Title.Should().Be("The Man Who Died Twice");
+            getBookResponse.Data.Output.Author.Should().Be("Richard Osman");
+        }
+    }
+}
